9ecb303d6a4d52dce315253c02a39894abd6b55c
Jonaira Manha <abcd@Jonaeath.local>
2023-12-10 22:03:58 +0600

    add switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Button} from '@chakra-ui/react'
+import { Grid, GridItem} from '@chakra-ui/react'
+import Navbar from './component/Sharedfile/Navbar/Navbar'
 
 function App() {
 
   return (
-    <>
-      <Button colorScheme='blue'>Button</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg: '250px 1fr'
+      }}
+    >
+    <GridItem bg='orange.300' area={'nav'}>
+      <Navbar/>
+    </GridItem>
+    <GridItem  paddingX={5} area={'aside'}>
+      aside
+    </GridItem>
+    <GridItem area={'main'}>
+      Main
+    </GridItem>
+  </Grid>
   )
 }
 
